a310bf52463baa455f4aec93013c0e3bb98e654f
Nikita Chumak <nv4umak@gmail.com>
2026-03-26 14:55:26 +0100

    changed names

--- src/components/Final.jsx
+++ src/components/Final.jsx
@@ -5,7 +5,7 @@ export default function Final() {
     <div className="final-scene">
       <img src="/card2.png" alt="Обязательно сбудется!" className="final-img" />
       <div className="final-caption">
-        <p className="final-caption__line final-caption__line--1">Обязательно сбудится!</p>
+        <p className="final-caption__line final-caption__line--1">Обязательно сбудется!</p>
         <p className="final-caption__line final-caption__line--2">Крепко тебя обнимаем!</p>
       </div>
     </div>
